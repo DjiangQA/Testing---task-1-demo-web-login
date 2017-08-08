bb4fa78165ff1d8a89909d95c1422c6040db7724
david.jiang <david.jiang@hotmail.com>
2017-08-08 17:16:49 +0100

    6th commit

--- src/test/Test1.java
+++ src/test/Test1.java
@@ -2,6 +2,7 @@ package test;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.IOException;
 import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
@@ -15,12 +16,24 @@ import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Wait;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+
+
 public class Test1 {
 
 	private Navigation N;
 	private RegisterPage R;
 	private LoginPage L;
 	private WebDriver CMDR;
+	private ScreenShot S;
+	
+	private ExtentReports report;
+    private ExtentTest test;
+    private String reportFilePath = "Report.html";
+    
 	
 	@Before
 	public void setUp() {
@@ -30,16 +43,18 @@ public class Test1 {
 		N = PageFactory.initElements(CMDR, Navigation.class);
 		R = PageFactory.initElements(CMDR, RegisterPage.class);
 		L = PageFactory.initElements(CMDR, LoginPage.class);
+		
 	}
 
 	@Test
-	public void test() throws InterruptedException {
+	public void test() throws InterruptedException, IOException {
 		String userName = "test";
 		String passWord = "test";
 		
+		
 		Wait<WebDriver> wait = new FluentWait<WebDriver>(CMDR)
 				.withTimeout(30, TimeUnit.SECONDS)
-				.pollingEvery(5, TimeUnit.SECONDS)
+				.pollingEvery(100, TimeUnit.MILLISECONDS)
 				.ignoring(NoSuchElementException.class);
 
 
@@ -65,6 +80,7 @@ public class Test1 {
 		//Thread.sleep(2000);
 		
 		R.enterPassword(passWord);
+		
 		R.clickSubmit();
 		//Thread.sleep(2000);
 		
@@ -111,14 +127,33 @@ public class Test1 {
 
 		WebElement check = CMDR
 				.findElement(By.xpath("/html/body/table/tbody/tr/td[1]/big/blockquote/blockquote/font/center/b"));
-		Thread.sleep(2500);
 		assertEquals("Text does not match", check.getText(), "**Successful Login**");
+		
+		
+		report = new ExtentReports();
+        ExtentHtmlReporter extentHtmlReporter = new ExtentHtmlReporter(reportFilePath);
+        extentHtmlReporter.config().setReportName("Test1_report");
+        extentHtmlReporter.config().setDocumentTitle("Test1_report");
+        report.attachReporter(extentHtmlReporter);
+        test = report.createTest("Test1");
+        
+        
+        test.log(
+				Status.INFO,
+				"Error Snapshot : "
+						+ test.addScreenCaptureFromPath(ScreenShot.take(CMDR, "Report")));
+        
+        
+        
+        test.log(Status.INFO,"Info level");
 
 	}
 
 	@After
 	public void after() {
+		
 		CMDR.quit();
+		report.flush();
 	}
 
 }
